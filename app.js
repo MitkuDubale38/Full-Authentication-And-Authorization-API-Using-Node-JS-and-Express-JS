887e06a60b2627c57dbbe04a751ce5eb34687a86
Mitku Dubale <60028209+MitkuDubale38@users.noreply.github.com>
2023-05-07 16:59:35 +0000

    added change password functionality

--- app.js
+++ app.js
@@ -6,8 +6,9 @@ const app = express();
 app.use(express.json());
 
 //registering routes
-app.use("/api/register", require("./routes/login"));
-app.use("/api/login", require("./routes/register"));
+app.use("/api/register", require("./routes/register"));
+app.use("/api/login", require("./routes/login"));
+app.use("/api/changePassword", require("./routes/change_password"));
 app.use("/api/home", require("./routes/protected_route_example"));
 
 module.exports = app;
