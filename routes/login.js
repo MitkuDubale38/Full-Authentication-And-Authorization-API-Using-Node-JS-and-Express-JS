ae9a0563e0631fc1bea2279bc0845d44d3f454f9
Mitku Dubale <60028209+MitkuDubale38@users.noreply.github.com>
2023-05-07 20:56:25 +0000

    Update login.js

--- routes/login.js
+++ routes/login.js
@@ -33,8 +33,9 @@ loginRouter.post("/", async (req, res) => {
       user.token = token;
       user.refreshToken = refreshToken;
       res.status(200).json(user);
+    } else {
+      res.status(400).send("Invalid Credentials");
     }
-    res.status(400).send("Invalid Credentials");
   } catch (err) {
     console.log(err);
   }
