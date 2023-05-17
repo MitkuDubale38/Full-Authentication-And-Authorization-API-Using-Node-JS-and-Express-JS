a15d5e5949167fd820f631eb1dbf3bf4af5b7161
Mitku Dubale <60028209+MitkuDubale38@users.noreply.github.com>
2023-05-17 18:09:26 +0000

    generating swagger doc using auto gen tool progress

--- routes/register.js
+++ routes/register.js
@@ -4,53 +4,7 @@ require("dotenv").config();
 const User = require("../models/users");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-/**
- * @swagger
- * components:
- *   schemas:
- *     Signup:
- *       type: object
- *       required:
- *         - first_name
- *         - last_name
- *         - email
- *         - password
- *       properties:
- *         first_name:
- *           type: string
- *           description: email of the user
- *         last_name:
- *           type: string
- *           description: email of the user
- *         email:
- *           type: string
- *           description: email of the user
- *         password:
- *           type: String
- *           description: password of the user
- */
-/**
- * @swagger
- * /register:
- *   post:
- *     tags: [Signup]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *             $ref: '#/components/schemas/Signup'
- *     responses:
- *       201:
- *         description: registration Successfull.
- *         content:
- *           application/json:
- *             schema:
- *               $ref: '#/components/schemas/Signup'
- *       500:
- *         description: Server error occured
- *
- */
+
 //register
 registrationRouter.post("/", async (req, res) => {
   try {
