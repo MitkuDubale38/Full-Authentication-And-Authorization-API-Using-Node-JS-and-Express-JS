144fd30db8fa4957d81c851709f472a534472d02
Mitku Dubale <60028209+MitkuDubale38@users.noreply.github.com>
2023-05-19 10:14:45 +0300

    Update forget_password.js

--- routes/forget_password.js
+++ routes/forget_password.js
@@ -9,7 +9,7 @@ const nodemailer = require("nodemailer");
 let transporter = nodemailer.createTransport({
   host: "smtp.gmail.com",
   port: 587,
-  secure: false, // true for 465, false for other ports
+  secure: true, // true for 465, false for other ports
   auth: {
     user: "mitedubale3864@gmail.com", // generated ethereal user
     pass:  process.env.EMAIL_PASSWORD, // generated ethereal password
