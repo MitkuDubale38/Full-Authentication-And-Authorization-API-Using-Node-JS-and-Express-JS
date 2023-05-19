c3549d34f8db1f0be57de654731f169cb99f141a
Mitku Dubale <60028209+MitkuDubale38@users.noreply.github.com>
2023-05-19 09:48:24 +0300

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -6,13 +6,13 @@ const verifyToken = (req, res, next) => {
     req.headers["x-access-token"];
 
   if (!token) {
-    return res.status(403).send("A token is required for authentication");
+    return res.status(403).json("A token is required for authentication");
   }
   try {
     const decoded = jwt.verify(token, config.TOKEN_KEY);
     req.user = decoded;
   } catch (err) {
-    return res.status(401).send("Invalid Token");
+    return res.status(401).json("Invalid Token");
   }
   return next();
 };
